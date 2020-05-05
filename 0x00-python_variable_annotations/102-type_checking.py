16493069b5539c7aa59c33edcecfa462be39b91d
arturadamian <arturadamian@gmail.com>
2020-05-04 23:52:18 -0700

    initial commit

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" Type Checking
+"""
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """ add type annotations to the function
+        Args:
+            lst: Tuple
+            factor: int
+        Returns: List
+    """
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
