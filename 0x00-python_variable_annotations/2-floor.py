a2cf80cdc5d166013c2964a19efe9fe0bc1c2127
arturadamian <arturadamian@gmail.com>
2020-05-04 20:40:02 -0700

    initial commit

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""  Basic annotations - floor
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """ returns returns the floor of the float
+        Args:
+            n: float
+        Returns: int
+    """
+    return math.floor(n)
