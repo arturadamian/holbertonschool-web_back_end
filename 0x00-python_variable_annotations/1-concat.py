c9105fd4406ccda31a380423d2e786e13a02452f
arturadamian <arturadamian@gmail.com>
2020-05-04 20:26:12 -0700

    initial commit

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""  Basic annotations - concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ returns concatinated strings
+        Args:
+            str1: first string
+            str2: second string
+        Returns: concat of str1 and str2 (str)
+    """
+    return str1 + str2
