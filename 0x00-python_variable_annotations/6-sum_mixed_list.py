55086ada7b4fd6d2158225eb3cb15e41bbadbd83
arturadamian <arturadamian@gmail.com>
2020-05-04 21:28:42 -0700

    a typo?

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,7 +4,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ returns the sum of elements of the list as a float.
         Args:
             mxd_lst: List[Union[float, int]]
