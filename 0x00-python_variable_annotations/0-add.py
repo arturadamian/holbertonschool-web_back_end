b74e236070e07b0295b9787cf8735421faf20dd9
arturadamian <arturadamian@gmail.com>
2020-05-04 20:20:49 -0700

    documenting

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
+"""  Basic annotations
+"""
 
 
-#  Basic annotations
 def add(a: float, b: float) -> float:
+    """ returns the sum of floats
+        Args:
+            a: first float
+            b: second float
+        Returns: sum of a and b (float)
+    """
     return a + b
