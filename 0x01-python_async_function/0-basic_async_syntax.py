2f68591cefaf4596deb6f6ed8794c931cf351ab3
arturadamian <arturadamian@gmail.com>
2020-05-05 14:26:19 -0700

    no typing in the middle

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """ asynchronous coroutine that takes in an argument
         (max_delay, with a default value of 10) named wait_random
         that waits for a random delay between 0 and max_delay
@@ -13,6 +13,6 @@ async def wait_random(max_delay: int = 10):
             max_delay: int = 10
         Returns: random float
     """
-    random_float: float = random.uniform(0, max_delay)
+    random_float = random.uniform(0, max_delay)
     await asyncio.sleep(random_float)
     return random_float
