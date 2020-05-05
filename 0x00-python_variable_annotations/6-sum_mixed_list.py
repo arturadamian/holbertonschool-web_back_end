0b929db23d0a491e7176f6f8ee337b6d06227302
arturadamian <arturadamian@gmail.com>
2020-05-04 21:25:17 -0700

    documenting

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""  Complex types - list of floats
+""" Complex types - mixed list
 """
 from typing import List, Union
 
@@ -7,8 +7,7 @@ from typing import List, Union
 def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
     """ returns the sum of elements of the list as a float.
         Args:
-            mxd_lst: List[float, int]
+            mxd_lst: List[Union[float, int]]
         Returns: float
     """
     return sum(mxd_lst)
-    
