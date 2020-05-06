151a96235a984a6671af3172c30bdc116f0f1a9a
arturadamian <arturadamian@gmail.com>
2020-05-05 20:42:19 -0700

    create_task add

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,6 +3,7 @@
     at the same time with async
 """
 import bisect
+import asyncio
 from typing import List
 
 
@@ -10,8 +11,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """return the list of all the delays in ascending order"""
-    list_of_delays = []
+    list_of_delays: List[float] = []
     for _ in range(n):
-        x = await wait_random(max_delay)
-        bisect.insort(list_of_delays, x)
+        x = asyncio.create_task(wait_random(max_delay))
+        bisect.insort(list_of_delays, await x)
     return list_of_delays
