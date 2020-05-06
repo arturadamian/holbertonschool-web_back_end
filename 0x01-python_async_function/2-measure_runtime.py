7a6cbb569e6bafafea9103d4f7b5df1fdff8814a
arturadamian <arturadamian@gmail.com>
2020-05-05 22:52:09 -0700

    delete the last line

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
-""" Let's execute multiple coroutines
-    at the same time with async
+""" Measure the runtime
 """
-import time
 import asyncio
+import time
 
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """return the list of all the delays in ascending order"""
+    """measures the total execution time"""
     start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
     total_time = time.time() - start_time
