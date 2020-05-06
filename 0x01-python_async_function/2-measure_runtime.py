152d438f180db2555618486a615c36575bdb36b2
arturadamian <arturadamian@gmail.com>
2020-05-05 22:46:51 -0700

    initial commit

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Let's execute multiple coroutines
+    at the same time with async
+"""
+import time
+import asyncio
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """return the list of all the delays in ascending order"""
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    total_time = time.time() - start_time
+    return total_time / n
