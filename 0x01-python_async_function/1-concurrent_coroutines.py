402469a274c065e2057a3a583552edd5bde42e93
arturadamian <arturadamian@gmail.com>
2020-05-05 22:28:14 -0700

    had wrong typing in args

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,9 +9,9 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def wait_n(n: int, max_delay: float) -> List[float]:
     """return the list of all the delays in ascending order"""
-    list_of_delays = []
+    list_of_delays: List[float] = []
     for _ in range(n):
         bisect.insort(list_of_delays, await wait_random(max_delay))
     return list_of_delays
