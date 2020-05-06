c1fe9be0118c615ac578ad93cbad4ae5e74bf054
arturadamian <arturadamian@gmail.com>
2020-05-05 20:16:37 -0700

    fix typing

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,12 +3,12 @@
     at the same time with async
 """
 import bisect
-import asyncio
+from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     # return the list of all the delays in ascending order
     list_of_delays = []
     for _ in range(n):
