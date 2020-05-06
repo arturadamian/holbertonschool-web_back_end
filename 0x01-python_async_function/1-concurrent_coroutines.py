facb7c88d007ea6148a2b5b8143f48a8af20277b
arturadamian <arturadamian@gmail.com>
2020-05-05 20:20:03 -0700

    fix the loop

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,8 +9,9 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    # return the list of all the delays in ascending order
+    """return the list of all the delays in ascending order"""
     list_of_delays = []
     for _ in range(n):
-        bisect.insort(list_of_delays, await wait_random(max_delay))
+        x = await wait_random(max_delay)
+        bisect.insort(list_of_delays, x)
     return list_of_delays
