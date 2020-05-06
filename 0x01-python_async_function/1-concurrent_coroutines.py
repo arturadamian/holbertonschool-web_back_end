c7f9e37a080ca5fd0e6bde43ca4a0adf49255673
arturadamian <arturadamian@gmail.com>
2020-05-05 21:58:30 -0700

    another try

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -13,6 +13,5 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """return the list of all the delays in ascending order"""
     list_of_delays: List[float] = []
     for _ in range(n):
-        x = asyncio.create_task(wait_random(max_delay))
-        bisect.insort(list_of_delays, await x)
+        bisect.insort(list_of_delays, await wait_random(max_delay))
     return list_of_delays
