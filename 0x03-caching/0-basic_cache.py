bcfbc6c221b9c3669ec6a0b445217fd9a0fbc4d0
arturadamian <arturadamian@gmail.com>
2020-05-10 17:48:46 -0700

    initial commit

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Basic Cache module
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ inherits from BaseCaching and is a caching system
+    """
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        return self.cache_data.get(key)
