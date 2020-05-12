0bfc4865e8694ee2d34c16a22b17f99651154cce
arturadamian <arturadamian@gmail.com>
2020-05-11 21:23:53 -0700

    initial commit

--- 0x03-caching/2-lifo_cache.py
+++ 0x03-caching/2-lifo_cache.py
@@ -8,22 +8,18 @@ class LIFOCache(BaseCaching):
     """
     def __init__(self):
         super().__init__()
-        self.lifo_list = []
+        self.last_key = ''
 
     def put(self, key, item):
         """ Add an item in the cache
         """
-        print(self.lifo_list)
+        print(self.cache_data)
         if key and item:
-            if key in self.cache_data:
-                self.cache_data[key] = item
-            else:
-                if len(self.cache_data) > BaseCaching.MAX_ITEMS - 1:
-                    print("DISCARD: {}".format(self.lifo_list[3]))
-                    del self.cache_data[self.lifo_list[3]]
-                    self.lifo_list.pop(3)
-                self.cache_data[key] = item
-                self.lifo_list.append(key)
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                print("DISCARD: {}".format(self.last_key))
+                self.cache_data.pop(self.last_key)
+            self.last_key = key
 
     def get(self, key):
         """ Get an item by key
