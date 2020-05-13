9f86acff8d8c28495352fbcd5ba822febbc1f13d
arturadamian <arturadamian@gmail.com>
2020-05-13 15:10:19 -0700

    initial commit

---
+++ 0x04-pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env pyhton3
+"""return a tuple of size two containing a start index
+    and an end index corresponding to the range of indexes
+"""
+
+
+def index_range(page, page_size):
+    if page and page_size:
+        start_index = (page - 1) * page_size if page > 1 else 0
+        end_index = page_size * page
+    return (start_index, end_index)
