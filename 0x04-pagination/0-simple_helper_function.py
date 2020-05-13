c620513a3c6ff761d8e5e012e69c2b2402ca4c3f
arturadamian <arturadamian@gmail.com>
2020-05-13 15:18:22 -0700

    documenting

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -1,11 +1,13 @@
-#!/usr/bin/env pyhton3
-"""return a tuple of size two containing a start index
-    and an end index corresponding to the range of indexes
+#!/usr/bin/env python3
+""" Simple helper function
 """
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ return a tuple of size two containing a start index
+        and an end index corresponding to the range of indexes
+    """
     if page and page_size:
         start_index: int = (page - 1) * page_size
         end_index: int = start_index + page_size
