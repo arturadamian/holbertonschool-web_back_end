cf3d1def587e0748e7d4908bcaf28730d7227579
arturadamian <arturadamian@gmail.com>
2020-05-18 21:49:22 -0700

    add checking function

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -8,3 +8,9 @@ import bcrypt
 def hash_password(pw: str) -> bytes:
     """returns a salted, hashed password, which is a byte string"""
     return bcrypt.hashpw(pw.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(h_password: bytes, password: str) -> bool:
+    """validates that the provided password matches the hashed password
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), h_password)
