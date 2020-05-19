efcec73bf61353c9a33a1e155436e8f48e8fb6ba
arturadamian <arturadamian@gmail.com>
2020-05-18 20:48:24 -0700

    add typing plus change set environ to get environ

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -53,10 +53,10 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """returns a connector to the database"""
     return mysql.connector.connect(
-                        host=os.environ['PERSONAL_DATA_DB_HOST'],
-                        database=os.environ.get('PERSONAL_DATA_DB_NAME'),
-                        user=os.environ['PERSONAL_DATA_DB_USERNAME'],
-                        password=os.environ['PERSONAL_DATA_DB_PASSWORD'])
+                    host=os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost'),
+                    database=os.environ.get('PERSONAL_DATA_DB_NAME', 'root'),
+                    user=os.environ.get('PERSONAL_DATA_DB_USERNAME'),
+                    password=os.environ.get('PERSONAL_DATA_DB_PASSWORD', ''))
