1c1a49891a62f6c1a0a112fa6079e6be908b6a05
arturadamian <arturadamian@gmail.com>
2020-05-22 21:57:04 -0700

    complete Basic authentication

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -67,3 +67,13 @@ class BasicAuth(Auth):
                 if user.is_valid_password(user_pwd):
                     return user
             return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ overloads Auth and
+            retrieves the User instance for a request
+        """
+        header = self.authorization_header(request)
+        base64_header = self.extract_base64_authorization_header(header)
+        decode_header = self.decode_base64_authorization_header(base64_header)
+        email, pwd = self.extract_user_credentials(decode_header)
+        return self.user_object_from_credentials(email, pwd)
