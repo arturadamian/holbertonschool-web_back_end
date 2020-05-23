7f77f3617f06f18f6658895af4c70cf2d1acc1b3
arturadamian <arturadamian@gmail.com>
2020-05-22 23:43:50 -0700

    rewrite the method

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -58,6 +58,7 @@ class BasicAuth(Auth):
         for user in users:
             if user.is_valid_password(user_pwd):
                 return user
+        return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ overloads Auth and
