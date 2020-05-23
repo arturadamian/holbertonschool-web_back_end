8113465ef9781a3b99db26b049862caf6300b0e9
arturadamian <arturadamian@gmail.com>
2020-05-22 20:44:51 -0700

    class name typo

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -3,7 +3,7 @@ from flask import request
 from typing import List, TypeVar
 
 
-class Auth:
+class Auth():
     """manage the API authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require authorithation"""
