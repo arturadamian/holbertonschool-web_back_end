e90b62b7732da37dfc703582e3b47697d2ad1caf
arturadamian <arturadamian@gmail.com>
2020-05-25 20:25:14 -0700

    intitial commit

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ API authentication
 """
+from api.v1.auth.auth import Auth
 from flask import request
 from typing import List, TypeVar
 
 
 class SessionAuth(Auth):
-    """ manages the API authentication"""
-    self.issubclass(SessionAuth, Auth)
+    """ Session Auth class"""
