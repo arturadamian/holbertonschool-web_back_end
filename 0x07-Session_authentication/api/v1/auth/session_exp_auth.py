4e2c6b15b6643989e68dd308c6b2fe4d1c79f0b3
arturadamian <arturadamian@gmail.com>
2020-05-27 22:59:52 -0700

    add documentation

--- 0x07-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
-""" API authentication
+""" Session Experation Authentication Module
 """
 from api.v1.auth.session_auth import SessionAuth
-from flask import request
-from typing import List, TypeVar
 from datetime import datetime, timedelta
-import uuid
-from models.user import User
 from os import getenv
 
 
 class SessionExpAuth(SessionAuth):
-    """ Session Exp Auth class"""
+    """ Session Experation Authentication Class
+        Methods:
+            create_session - add time return session id
+            user_id_for_session_id - according to time
+                return user id
+            """
     def __init__(self):
         """Constructor"""
         s_duration = getenv('SESSION_DURATION')
@@ -21,7 +22,7 @@ class SessionExpAuth(SessionAuth):
             self.session_duration = 0
 
     def create_session(self, user_id=None):
-        """Create a Session ID by calling super()"""
+        """add time return session id"""
         if not user_id:
             return
         session_id = super().create_session(user_id)
@@ -38,7 +39,7 @@ class SessionExpAuth(SessionAuth):
         return session_id
 
     def user_id_for_session_id(self, session_id=None):
-        """Overload """
+        """according to time return user id """
         if not session_id:
             return
         session_dictionary = self.user_id_by_session_id.get(session_id)
