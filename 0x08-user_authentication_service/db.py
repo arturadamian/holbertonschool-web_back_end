26f9053e498a8297e6655e0cbcf4658fb123179f
arturadamian <arturadamian@gmail.com>
2020-06-02 08:33:11 -0700

    initial commit

---
+++ 0x08-user_authentication_service/db.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" Database module
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from typing import TypeVar
+from user import Base, User
+
+
+class DB:
+    """ Database class
+        Creates engine, session, adds user object to DB
+    """
+    def __init__(self):
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self):
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+        """save the user to the database"""
+        u = User(email=email, hashed_password=hashed_password)
+        self._session.add(u)
+        self._session.commit()
+        return u
