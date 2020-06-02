f08fe8512b8c631011bdf9dfc034cb1cb3fddcce
arturadamian <arturadamian@gmail.com>
2020-06-02 07:35:41 -0700

    initial commit

---
+++ 0x08-user_authentication_service/user.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""User model """
+
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///:memory:', echo=True)
+Base = declarative_base()
+
+
+class User(Base):
+    """ User class"""
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
