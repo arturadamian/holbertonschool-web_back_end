03acd29f8ed1c004337b6f9fe66975c101876db9
arturadamian <arturadamian@gmail.com>
2020-06-02 07:55:54 -0700

    missing attributes

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -14,9 +14,10 @@ class User(Base):
         User class
         Create a SQLAlchemy model named User for a database table named users
     """
-    __tablename__ = "users"
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
