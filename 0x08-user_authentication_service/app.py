ebae4bffab4fecd866d269e71426768961c27ed4
arturadamian <arturadamian@gmail.com>
2020-06-03 10:14:43 -0700

    register endpoint

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -3,15 +3,26 @@
 Route module for the API
 """
 from flask import Flask, jsonify
-
+from auth import Auth
 
 app = Flask(__name__)
-
+AUTH = Auth()
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello() -> str:
     """welcome msg"""
     return jsonify({"message": "Bienvenue"}), 200
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """registering the user"""
+    email = request.form('email')
+    password = request.form('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({{"email": {email}, "message": "user created"}), 200
+    except Exception:
+        return jsonify({"message": "email already registered"})
 
 
 if __name__ == "__main__":
