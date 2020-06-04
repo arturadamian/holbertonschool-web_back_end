bf87b439669f3e10a357c4f6e287fea83efd4018
arturadamian <arturadamian@gmail.com>
2020-06-03 22:29:41 -0700

    find user #12

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -55,3 +55,9 @@ class Auth:
             session_id = _generate_uuid()
             self._db.update_user(user.id, session_id=session_id)
             return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """Find user by session ID"""
+        if session_id:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
