50395633c0386bf38d43fd2238196befb903a43c
arturadamian <arturadamian@gmail.com>
2020-06-03 23:15:12 -0700

    add attr check

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -72,9 +72,10 @@ class Auth:
             update the user’s reset_token database field
             Return: the token
         """
-        user = self._db.find_user_by(email=email)
-        if not email:
-            raise ValueError
-        reset_token = _generate_uuid()
-        self._db.update_user(user.id, reset_token=reset_token)
-        return reset_token
+        if email:
+            user = self._db.find_user_by(email=email)
+            if not user:
+                raise ValueError
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
