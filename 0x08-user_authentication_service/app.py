317beed93bf1ba4f95817eb7e3f41afba2be8e14
arturadamian <arturadamian@gmail.com>
2020-06-03 23:02:47 -0700

    #15 initial commit

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -56,5 +56,16 @@ def logout():
     return redirect('/')
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile():
+    """response to the DELETE /sessions route"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
