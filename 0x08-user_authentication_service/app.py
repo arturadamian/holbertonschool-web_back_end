cf5b48673608f3adca88f7768984aa069ea0775b
arturadamian <arturadamian@gmail.com>
2020-06-03 23:40:24 -0700

    #19 initial commit

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -33,7 +33,7 @@ def users() -> str:
 
 
 @app.route('/sessions', methods=['POST'], strict_slashes=False)
-def login() -> str:
+def login():
     """respond to the POST /sessions route"""
     email = request.form.get('email')
     password = request.form.get('password')
@@ -57,7 +57,7 @@ def logout():
 
 
 @app.route('/profile', methods=['GET'], strict_slashes=False)
-def profile():
+def profile() -> str:
     """response to the DELETE /sessions route"""
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
@@ -74,7 +74,22 @@ def get_reset_password_token() -> str:
     reset_token = AUTH.get_reset_password_token(email)
     if not reset_token:
         abort(403)
-    return jsonify({"email": email, "reset_token": reset_token}), 200
+    else:
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+
+
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    """Update the password"""
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+    reset_token = AUTH.get_reset_password_token(email)
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except Exception:
+        abort(403)
 
 
 if __name__ == "__main__":
