78e93590aca7e77f1328b855fb1346490c830090
arturadamian <arturadamian@gmail.com>
2020-06-03 22:12:26 -0700

    log in

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Route module for the API
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -13,10 +13,10 @@ AUTH = Auth()
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello() -> str:
     """welcome msg"""
-    return jsonify({"message": "Bienvenue"}), 200
+    return jsonify({"message": "Bienvenue"})
 
 
-@app.route('/users', methods=['POST'], strict_slashes=False)
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
 def users() -> str:
     """registering the user"""
     email = request.form.get('email')
@@ -28,6 +28,19 @@ def users() -> str:
         return jsonify({"email": {email}, "message": "user created"}), 200
     except Exception:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app_views.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """respond to the POST /sessions route"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": {email}, "message": "logged in"})
+    response.set_cookies('session_id', session_id)
+    return response
 
 
 if __name__ == "__main__":
