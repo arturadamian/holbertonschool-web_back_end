b9f8cee7dea1423112d9ebf06c45053ae9a5b075
arturadamian <arturadamian@gmail.com>
2020-06-03 23:22:27 -0700

    #17 initial commit

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -67,5 +67,15 @@ def profile():
         abort(403)
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token():
+    """generate a token and respond with a 200 HTTP status"""
+    email = request.form.get('email')
+    reset_token = AUTH.get_reset_password_token(email)
+    if not reset_token:
+        abort(403)
+    return jsonify({"email": email, "reset_token": reset_token}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
