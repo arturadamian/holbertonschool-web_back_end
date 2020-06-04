5992169a703a58ef0524f65b47fcb64866fbf2b1
arturadamian <arturadamian@gmail.com>
2020-06-03 23:31:58 -0700

    #18 initial commit

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -79,3 +79,16 @@ class Auth:
             reset_token = _generate_uuid()
             self._db.update_user(user.id, reset_token=reset_token)
             return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ update the user’s hashed_password field with the new
+            hashed password and the reset_token field to None
+        """
+        if reset_token and password:
+            user = self._db.find_user_by(reset_token=reset_token)
+            if user:
+                hashed_password = _hash_password(password)
+                self._db.update_user(user.id, password=hashed_password,
+                                     reset_token=None)
+            else:
+                raise ValueError
