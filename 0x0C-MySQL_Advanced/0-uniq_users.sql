6de87ac93d27edfc1d1e1eda2902bad9003e8b40
arturadamian <arturadamian@gmail.com>
2020-06-19 19:04:48 -0700

    relocate primary key

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 --creates a table users
 CREATE TABLE If NOT EXISTS users (  
     id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY ( id ),
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    PRIMARY KEY ( id )
+    name VARCHAR(255)
 );
