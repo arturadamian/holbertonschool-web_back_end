a2023a9b77172cd22c8bb0741af74fe6dce1dee3
arturadamian <arturadamian@gmail.com>
2020-06-19 19:13:47 -0700

    two lines of comments

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
---creates a table users
-CREATE TABLE If NOT EXISTS users (  
-    id INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY ( id ),
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+-- creates a table users
+-- script can be executed on any database
+CREATE TABLE If NOT EXISTS `users` (  
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255)
 );
