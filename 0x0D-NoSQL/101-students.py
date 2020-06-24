2ccab34788661eeac418c61f5d101c37e6e0b4c8
arturadamian <arturadamian@gmail.com>
2020-06-23 23:57:24 -0700

    #101 initial commit

---
+++ 0x0D-NoSQL/101-students.py
@@ -0,0 +1,21 @@
+#!/uisr/bin/env python3
+"""pumongo aggregate"""
+
+
+def top_students(mongo_collection):
+    """returns all students sorted by average score"""
+    return mongo_collection.aggregate([
+        {
+            "$project":
+                {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+        },
+        {
+            "$sort":
+                {
+                    "averageScore": -1
+                }
+        }
+    ])
