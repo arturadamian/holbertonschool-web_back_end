000c006ed2054e862dca0caf0598fdfee92885b1
arturadamian <arturadamian@gmail.com>
2020-06-29 23:16:33 -0700

    spacing

--- 0x0E-ES6_basic/101-iterateThroughObject.js
+++ 0x0E-ES6_basic/101-iterateThroughObject.js
@@ -1,10 +1,9 @@
 export default function iterateThroughObject(reportWithIterator) {
-    let x = reportWithIterator.next();
-    let res = '';
-    while (!x.done) {
-      res += `${x.value} | `;
-      x = reportWithIterator.next();
-    }
-    return res.slice(0, res.length - 3);
+  let x = reportWithIterator.next();
+  let res = '';
+  while (!x.done) {
+    res += `${x.value} | `;
+    x = reportWithIterator.next();
   }
-  
+  return res.slice(0, res.length - 3);
+}
