9f6d72acdbf183e7998b6bee41c44df01b36b6a7
arturadamian <arturadamian@gmail.com>
2020-07-01 12:46:07 -0700

    6 initial commit

---
+++ 0x0F-ES6_promise/6-all-reject.js
@@ -0,0 +1,10 @@
+import signUpUser from './4-all-reject';
+import uploadPhoto from './5-all-reject';
+
+export default handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled(signUpUser(firstName, lastName), uploadPhoto(fileName))
+    .then((results) => results.map((result) => ({
+    status: result.status,
+    value: result.value,
+    })));
+}
