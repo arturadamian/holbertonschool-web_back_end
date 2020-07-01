82c1760058b4bfcc4290b8fc18c0263db82c61ad
arturadamian <arturadamian@gmail.com>
2020-07-01 10:17:33 -0700

    eslint'

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,15 +1,14 @@
-import { uploadPhoto, createUser } from './utils,js'
+import { uploadPhoto, createUser } from './utils.js';
 
-function handleProfileSignup() {
+export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       console.log(
         values[0].body,
         values[1].firstName,
         values[1].lastName,
-        );
-      }
-    )
+      );
+    })
     .catch(() => console.log('Signup system offline'));
 }
         
