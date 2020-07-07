845bfcec39c5425d6ef05c78f1e1984778aa3eeb
arturadamian <arturadamian@gmail.com>
2020-07-06 21:30:07 -0700

    json

--- 0x10-ES6_classes/7-airport.js
+++ 0x10-ES6_classes/7-airport.js
@@ -1,10 +1,10 @@
-export default class Airport {
+class Airport {
   constructor(name, code) {
     this._name = name;
     this._code = code;
   }
 
   get [Symbol.toStringTag]() {
-    return `Airport [${this._code}] {_name: ${this._name}, _code: ${this._code}}`;
+    return `${JSON.stringify(Airport)}`;
   }
 }
