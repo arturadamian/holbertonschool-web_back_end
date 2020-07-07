307d1babf7c3653a7c2402ad9a2892564089236f
arturadamian <arturadamian@gmail.com>
2020-07-06 23:16:00 -0700

    eslint

--- 0x10-ES6_classes/100-evcar.js
+++ 0x10-ES6_classes/100-evcar.js
@@ -5,20 +5,17 @@ class EVCar extends Car {
     super(brand, motor, color);
     this._range = range;
   }
-  
+
   static get [Symbol.species]() {
     return Car;
   }
-  
+
   cloneCar() {
     const Species = this.constructor[Symbol.species];
     return new Species(this._brand, this._motor, this._color, this._range);
   }
 }
-
-const ec1 = new EVCar("Tesla", "Turbo", "Red", "250");
-
-const ec2 = ec1.cloneCar();
-
-console.log(ec1);
-console.log(ec2);
+// const ec1 = new EVCar("Tesla", "Turbo", "Red", "250");
+// const ec2 = ec1.cloneCar();
+// console.log(ec1);
+// console.log(ec2);
