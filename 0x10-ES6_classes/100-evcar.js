4234c4f4a85dd2d9dbeadb897f24c9e52ab6dc40
arturadamian <arturadamian@gmail.com>
2020-07-06 23:18:58 -0700

    export default

--- 0x10-ES6_classes/100-evcar.js
+++ 0x10-ES6_classes/100-evcar.js
@@ -1,6 +1,6 @@
 const Car = require('./10-car.js');
 
-class EVCar extends Car {
+export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
@@ -15,7 +15,4 @@ class EVCar extends Car {
     return new Species(this._brand, this._motor, this._color, this._range);
   }
 }
-// const ec1 = new EVCar("Tesla", "Turbo", "Red", "250");
-// const ec2 = ec1.cloneCar();
-// console.log(ec1);
-// console.log(ec2);
+
