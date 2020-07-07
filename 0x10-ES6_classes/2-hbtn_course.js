1acf776bd41a82b07fa845c3f4d7fd4e580e3497
arturadamian <arturadamian@gmail.com>
2020-07-06 21:42:51 -0700

    wrong function check

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -19,23 +19,23 @@ export default class HolbertonCourse {
   }
 
   get students() {
-    if (students.constructor !== Array || students.every((el) => typeof el === 'string')) {
-      throw TypeError('students must be an Array of Strings');
-    }
     return this._students;
   }
 
   set name(newName) {
-    if (typeof name !== 'string') throw TypeError('name must be a String');
+    if (typeof newName !== 'string') throw TypeError('name must be a String');
     this._name = newName;
   }
 
   set length(newLength) {
-    if (typeof length !== 'number') throw TypeError('length must be a Number');
+    if (typeof newLength !== 'number') throw TypeError('length must be a Number');
     this._length = newLength;
   }
 
   set students(newStudents) {
+    if (newStudents.constructor !== Array || newStudents.every((el) => typeof el === 'string')) {
+      throw TypeError('students must be an Array of Strings');
+    }
     this._students = newStudents;
   }
 }
