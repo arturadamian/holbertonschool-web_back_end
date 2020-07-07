77ac599bf659cc15d3a579819d8156b407e9f171
arturadamian <arturadamian@gmail.com>
2020-07-06 21:36:26 -0700

    es lint

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -1,24 +1,25 @@
-class HolbertonCourse {
+export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== String) throw TypeError('name must be a String');
-    if (typeof(length) !== Number) throw TypeError('length must be a Number');
+    if (typeof name !== 'string') throw TypeError('name must be a String');
+    if (typeof length !== 'number') throw TypeError('length must be a Number');
     if (students.constructor !== Array || students.every((el) => typeof el === 'string')) {
       throw TypeError('students must be an Array of Strings');
-    };
+    }
     this._name = name;
     this._length = length;
     this._students = students;
   }
+  
   get name() {
-    return this._name
+    return this._name;
   }
 
   get length() {
-    return this._length
+    return this._length;
   }
 
   get students() {
-    return this._students
+    return this._students;
   }
 
   set name(newName) {
