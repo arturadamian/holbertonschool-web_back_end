0e7ac45d2a77d724437bb9f53991d1a005cd79f7
arturadamian <arturadamian@gmail.com>
2020-07-08 21:45:04 -0700

    es lint

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
   if (position > length) throw new Error('Position outside range');
-let buffer = new ArrayBuffer(length);const int8BufferArray = new Int8Array(buffer, position, value);
+  const buffer = new ArrayBuffer(length);
+  const int8BufferArray = new Int8Array(buffer);
+  int8BufferArray[position] = value;
+  return int8BufferArray;
 }
