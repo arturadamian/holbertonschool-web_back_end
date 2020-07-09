d81d2dec44b508c83fdf70df7ec6d78a870d4e6a
arturadamian <arturadamian@gmail.com>
2020-07-08 22:57:45 -0700

    check for every el

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   let resultString = '';
   if (!startString || !startString.length) return resultString;
   set.forEach((el) => {
-    if (el.startsWith(startString)) resultString += `${el.slice(startString.length)}-`;
+    if (el && el.startsWith(startString)) resultString += `${el.slice(startString.length)}-`;
   });
   return resultString.slice(0, resultString.length - 1);
 }
