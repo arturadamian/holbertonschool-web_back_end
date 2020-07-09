46d121e617c7fa3667e2e56d00e69b352ab60d81
arturadamian <arturadamian@gmail.com>
2020-07-08 23:02:23 -0700

    set pairs

--- 0x11-ES6_data_manipulation/9-groceries_list.js
+++ 0x11-ES6_data_manipulation/9-groceries_list.js
@@ -1,4 +1,6 @@
 export default function groceriesList() {
+  const groceryArray = [['Apples', 10], ['Tomatoes', 10], ['Pasta', 1], ['Rice', 1], ['Banana', 5]];
   const groceryMap = new Map();
-  return groceryMap.set('Apples', 10, 'Tomatoes', 10, 'Pasta', 1, 'Rice', 1, 'Banana', 5);
+  for (const el of groceryArray) groceryMap.set(el[0], el[1]);
+  return groceryMap;
 }
