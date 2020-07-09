d43bf7e7db9c4fd27e60ca957a57743d75d20cec
arturadamian <arturadamian@gmail.com>
2020-07-08 22:56:58 -0700

    const vs let

--- 0x11-ES6_data_manipulation/9-groceries_list.js
+++ 0x11-ES6_data_manipulation/9-groceries_list.js
@@ -1,4 +1,4 @@
 export default function groceriesList() {
-  let groceryMap = new Map();
+  const groceryMap = new Map();
   return groceryMap.set('Apples', 10, 'Tomatoes', 10, 'Pasta', 1, 'Rice', 1, 'Banana', 5);
 }
