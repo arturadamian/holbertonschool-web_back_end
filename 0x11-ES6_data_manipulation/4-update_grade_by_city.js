343d7a589badf7a6fcecee57ca6e77265354235f
arturadamian <arturadamian@gmail.com>
2020-07-08 19:12:25 -0700

    eslint

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -4,9 +4,8 @@ export default function getStudentsByLocation(array, city, grad) {
     .map((student) => {
       const gradeItem = grad
         .filter((item) => item.studentId === student.id)
-        .map((x) => x.grade)
-        [0];
-      const grade = (gradeItem) ? gradeItem : 'N/A';
-      return {...student, grade,};
+        .map((x) => x.grade)[0];
+      const grade = gradeItem || 'N/A';
+      return { ...student, grade };
     });
 }
