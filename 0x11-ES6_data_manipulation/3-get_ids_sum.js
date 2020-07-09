591d1980b3d62f4504683d57bb616621c51ce316
arturadamian <arturadamian@gmail.com>
2020-07-08 23:22:17 -0700

    extra comma

--- 0x11-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x11-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
 export default function getListStudentIds(array) {
-  return array.reduce((accumulator, item,) => accumulator + item.id, 0);
+  return array.reduce((accumulator, item) => accumulator + item.id, 0);
 }
