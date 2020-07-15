06cf5e39e26fae7d52cec422213c8da464b933c9
arturadamian <arturadamian@gmail.com>
2020-07-14 23:49:43 -0700

    5 initial commit

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -5,18 +5,18 @@ const port = 1245;
 
 const app = http.createServer((req, res) => {
   const {  method, url, headers } = req;
-  if (method === "GET" && url === "/") {
+  if (method === 'GET' && url === '/') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello Holberton School!');
   }
-  if (method === "GET" && url === "/students") {
+  if (method === 'GET' && url === '/students') {
     countStudents(String(process.argv.slice(2)))
       .then((arrayOfClasses) => {
         res.write(`This is the list of our students\n`);
-        res.write(`Number of students: ${arrayOfClasses['count']}\n`);
+        res.write(`Number of students: ${arrayOfClasses.count}\n`);
         for (const cls in arrayOfClasses) {
-            if (cls && cls !== 'count') res.write(`Number of students in ${cls}: ${arrayOfClasses[cls].length}. List: ${arrayOfClasses[cls].join(', ')}\n`);
+          if (cls && cls !== 'count') res.write(`Number of students in ${cls}: ${arrayOfClasses[cls].length}. List: ${arrayOfClasses[cls].join(', ')}\n`);
         }
         res.end();
       })
