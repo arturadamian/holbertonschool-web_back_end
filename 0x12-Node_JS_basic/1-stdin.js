3ee5090cefd776272fa87cd2520e598bc6807a55
arturadamian <arturadamian@gmail.com>
2020-07-14 18:36:11 -0700

    es lint

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,14 +1,11 @@
+const showName = (name) => {
+  if (name) console.log('Your name is: ' + name);
+}
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', function() {
   const name = String(process.stdin.read());
   showName(name);
 })
 
-const showName = (name) => {
-  if (name)
-    console.log('Your name is: ' + name);
-}
-const laFinale = () => {
-  if (process.stdout.isTTY)
-    process.stdout.write('This important software is now closing\n');
-}
+if (process.stdout.isTTY) process.stdout.write('This important software is now closing\n');
