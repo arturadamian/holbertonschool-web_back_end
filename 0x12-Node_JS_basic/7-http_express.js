329faf3941a2828848f9fd3ab19b7deae9ded6ff
arturadamian <arturadamian@gmail.com>
2020-07-14 23:57:11 -0700

    eslint

--- 0x12-Node_JS_basic/7-http_express.js
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => res.send('Hello Holberton School!'));
 app.get('/students', ((req, res) => {
   countStudents(String(process.argv.slice(2)))
     .then((arrayOfClasses) => {
-      res.write(`This is the list of our students\n`);
+      res.write('This is the list of our students\n');
       res.write(`Number of students: ${arrayOfClasses.count}\n`);
       for (const cls in arrayOfClasses) {
         if (cls && cls !== 'count') res.write(`Number of students in ${cls}: ${arrayOfClasses[cls].length}. List: ${arrayOfClasses[cls].join(', ')}\n`);
@@ -15,8 +15,7 @@ app.get('/students', ((req, res) => {
       res.end();
     })
     .catch((err) => { throw err; });
-  })
-);
+}),);
 app.listen(1245);
 
 module.exports = app;
