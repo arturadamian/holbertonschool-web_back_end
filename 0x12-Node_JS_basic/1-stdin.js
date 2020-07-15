04feb56d7a6178c14666449c258d28cbfa14c114
arturadamian <arturadamian@gmail.com>
2020-07-14 22:16:54 -0700

    no casting

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,13 +1,8 @@
-const showName = (name) => {
-  if (name) console.log(`Your name is: ${name}`);
-};
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
-  const name = String(process.stdin.read());
-  showName(name);
+  const name = process.stdin.read();
+  if (name) console.log(`Your name is: ${name}`);
 });
 
-process.stdin.on('end', () => {
-  if (process.stdout.isTTY) process.stdout.write('This important software is now closing\n');
-});
+if (process.stdout.isTTY)
+  process.stdin.on('end', () => process.stdout.write('This important software is now closing\n'));
