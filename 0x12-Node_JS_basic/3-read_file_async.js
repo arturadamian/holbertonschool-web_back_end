b30a711e98268b5676f008a81cd89bf4b73dd66a
arturadamian <arturadamian@gmail.com>
2020-07-14 21:52:39 -0700

    es lint

--- 0x12-Node_JS_basic/3-read_file_async.js
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -5,11 +5,11 @@ const countStudents = (path) => {
     fs.readFile(path, (err, data) => {
       if (err) rej(Error('Cannot load the database'));
       if (data) {
-        data = data.toString().split('\n');
-        data = data.slice(1, data.length - 1);
-        console.log(`Number of students: ${data.length}`);
+        let dt = data.toString().split('\n');
+        dt = dt.slice(1, dt.length - 1);
+        console.log(`Number of students: ${dt.length}`);
         const arrayOfClasses = {};
-        for (const row of data) {
+        for (const row of dt) {
           const student = row.split(',');
           if (!arrayOfClasses[student[3]]) arrayOfClasses[student[3]] = [];
           arrayOfClasses[student[3]].push(student[0]);
@@ -18,9 +18,9 @@ const countStudents = (path) => {
           if (cls) console.log(`Number of students in ${cls}: ${arrayOfClasses[cls].length}. List: ${arrayOfClasses[cls].join(', ')}`);
         }
       }
+      res();
     });
-    res();
-  }
+  };
   return new Promise(promise);
 };
 
