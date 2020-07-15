3324a47b7a7bf84f01ddc62dcfa5181eac3ac521
arturadamian <arturadamian@gmail.com>
2020-07-14 23:54:08 -0700

    eslint

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -4,7 +4,7 @@ const countStudents = require('./3-read_file_async');
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-  const {  method, url, headers } = req;
+  const { method, url } = req;
   if (method === 'GET' && url === '/') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
@@ -13,7 +13,7 @@ const app = http.createServer((req, res) => {
   if (method === 'GET' && url === '/students') {
     countStudents(String(process.argv.slice(2)))
       .then((arrayOfClasses) => {
-        res.write(`This is the list of our students\n`);
+        res.write('This is the list of our students\n');
         res.write(`Number of students: ${arrayOfClasses.count}\n`);
         for (const cls in arrayOfClasses) {
           if (cls && cls !== 'count') res.write(`Number of students in ${cls}: ${arrayOfClasses[cls].length}. List: ${arrayOfClasses[cls].join(', ')}\n`);
