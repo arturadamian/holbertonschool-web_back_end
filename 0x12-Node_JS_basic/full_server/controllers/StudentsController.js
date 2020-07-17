1bda502d5eab908c4b8c0f19a85084d7aa8b2833
arturadamian <arturadamian@gmail.com>
2020-07-16 23:56:14 -0700

    new line

--- 0x12-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x12-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -2,19 +2,18 @@ const readDatabase = require('../utils');
 
 class StudentsController {
  static getAllStudents(req, res) {
-    readDatabase('database.csv')
-      .then((arrayOfClasses) => {
-        res.status(200);
-        res.write(`Number of students: ${arrayOfClasses.count}\n`);
-        for (const cls in arrayOfClasses) {
-          if (cls && cls !== 'count') res.write(`Number of students in ${cls}: ${arrayOfClasses[cls].length}. List: ${arrayOfClasses[cls].join(', ')}\n`);
-        }
-        res.end();
-      })
-      .catch((err) => { 
-        res.status(500);
-        throw new Error('Cannot load the database');
-      });
+  readDatabase('database.csv')
+    .then((arrayOfClasses) => {
+      res.status(200);
+      res.write(`Number of students: ${arrayOfClasses.count}\n`);
+      for (const cls in arrayOfClasses) {
+        if (cls && cls !== 'count') res.write(`Number of students in ${cls}: ${arrayOfClasses[cls].length}. List: ${arrayOfClasses[cls].join(', ')}\n`);
+      }
+      res.end();
+    })
+    .catch((err) => { 
+      res.status(500);
+      throw new Error('Cannot load the database');
+    });
  }
- static
 }
