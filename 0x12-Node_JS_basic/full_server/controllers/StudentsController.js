cef2e46a16b1e6dad0b81d9555f1fb9d59e21f50
arturadamian <arturadamian@gmail.com>
2020-07-16 23:47:30 -0700

    initial commit 8

---
+++ 0x12-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,20 @@
+const readDatabase = require('../utils');
+
+class StudentsController {
+ static getAllStudents(request, response) {
+    readDatabase('database.csv')
+      .then((arrayOfClasses) => {
+        res.status(200);
+        res.write(`Number of students: ${arrayOfClasses.count}\n`);
+        for (const cls in arrayOfClasses) {
+          if (cls && cls !== 'count') res.write(`Number of students in ${cls}: ${arrayOfClasses[cls].length}. List: ${arrayOfClasses[cls].join(', ')}\n`);
+        }
+        res.end();
+      })
+      .catch((err) => { 
+        res.status(500);
+        throw new Error('Cannot load the database');
+      });
+ }
+ static
+}
