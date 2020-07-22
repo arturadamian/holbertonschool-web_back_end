409fb1a65abeed5611dd763f00f30e17578de0fc
arturadamian <arturadamian@gmail.com>
2020-07-21 23:17:52 -0700

    use equal

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -3,15 +3,15 @@ const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
   it('checks the output', () => {
-    assert.strictEqual(calculateNumber(1.1, 2.5), 4);
-    assert.strictEqual(calculateNumber(0.0, 0), 0);
+    assert.equal(calculateNumber(1.1, 2.5), 4);
+    assert.equal(calculateNumber(0.0, 0), 0);
   });
   it('negative numbers', () => {
-    assert.strictEqual(calculateNumber(-1, 1), 0);
-    assert.strictEqual(calculateNumber(-1.5, 0), -1);
+    assert.equal(calculateNumber(-1, 1), 0);
+    assert.equal(calculateNumber(-1.5, 0), -1);
   });
   it('checks arguments', () => {
-    assert.strictEqual(isNaN(calculateNumber(2.2)), true);
-    assert.strictEqual(isNaN(calculateNumber()), true);
+    assert.equal(isNaN(calculateNumber(2.2)), true);
+    assert.equal(isNaN(calculateNumber()), true);
   });
 });
